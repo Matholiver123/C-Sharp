cef1604028ad2d3312304dbfa0f5c55868288b7d
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-04-22 17:34:27 -0300

    Add files via upload

---
+++ ordenacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ordenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 5, 1, 8, 9 };
+
+            Array.Sort(numeros);
+
+            foreach (int i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

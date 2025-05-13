7015bcaff1d9566dcd1e967348061f1e630e9ff2
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-13 15:18:27 -0300

    Add files via upload

---
+++ MatrizDezporDezRandNum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MatrizDezporDezRandNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrixrRand = new int[10, 10];
+
+            Random rand = new Random();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matrixrRand[i, j] = rand.Next(100);
+                };
+            };
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(matrixrRand[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }   
+    }
+}

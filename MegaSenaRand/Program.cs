b5f236455f1471c4e90908c636f0577c60e0b830
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-13 15:18:58 -0300

    Add files via upload

---
+++ MegaSenaRand/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MatrizDezporDezRandNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrixMegaSenaRand = new int[2, 3];
+            Random rand = new Random();
+
+            Console.Write("MEGA SENA - ETEC Belém \nA seguir os números gerados do sorteio: \n \n");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrixMegaSenaRand[i, j] = rand.Next(1,61); 
+                    Console.Write(matrixMegaSenaRand[i, j] + "\t");
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

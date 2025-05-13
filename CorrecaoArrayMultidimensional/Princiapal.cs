d326e8d1975f500d2e21daf833a741ac3c30481f
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-05-13 15:17:09 -0300

    Add files via upload

---
+++ CorrecaoArrayMultidimensional/Princiapal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorrecaoArrayMultidimensional
+{
+    class Princiapal
+    {
+        static void Main(string[] args)
+        {
+            int colunas = 5;
+            int linhas = 5;
+            int[,] numeros = new int[colunas, linhas];
+
+            int numeroPar = 2;
+
+
+            for (int i = 0; i < colunas; i++) 
+            {
+                for (int j = 0; j < linhas; j++)
+                {
+                    numeros[i, j] = numeroPar;
+                    numeroPar += 2;
+                };
+            };
+
+            for (int i = 0; i < colunas; i++)
+            {
+                for (int j = 0; j < linhas; j++)
+                {
+                    Console.Write(numeros[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
